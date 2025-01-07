a14562c92da870acd9fdcc44b82deb2c967a6e04
Gaat99 <galoalcivar99@gmail.com>
2025-01-06 23:12:45 -0500

    LABORATORIO 8

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon (models.Model):
+    name = models.CharField(max_length=30, null=False) 
+    type = models.CharField(max_length=30, null=False) 
+    weight = models.DecimalField(decimal_places=4,max_digits=6) 
+    height = models.DecimalField(decimal_places=4,max_digits=6) 
+
+    def __str__(self):
+        return self.name
+
+class Trainer (models.Model):
+    name_trainer = models.CharField(max_length=30, null=False) 
+    last_name = models.CharField(max_length=30, null=False) 
+    level = models.IntegerField(default=1) 
+    birth = models.DateField(null=True, blank=True) 
+
+    def __str__(self):
+        return f"{self.name_trainer} - {self.last_name}"    
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,14 +1,29 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from .models import Pokemon
+from .models import Trainer
 def index(request):
-    pokemons = ['charmander', 'pikachu', 'squirtle']
+    pokemons = Pokemon.objects.all()
+    trainer = Trainer.objects.all()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render({'pokemons': pokemons}, request))
+    return HttpResponse(template.render({
+        'pokemons': pokemons,
+        'trainer': trainer
+        }, request))
 
-def pokemon(request, pokemon):
+def pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id = pokemon_id)
     template = loader.get_template('display_pokemon.html')
     context = {
         'pokemon': pokemon
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def trainer_details(request, trainer_id):
+    trainer = Trainer.objects.get(id = trainer_id)
+    template = loader.get_template('display_avatar.html')
+    context = {
+        'trainer': trainer
     }
     return HttpResponse(template.render(context, request))
